3ddb972459d5adbd1c6d49cfa2bb41a1d423d8fe
alexa-sl <>
2023-10-18 17:32:58 +0300

    cors

--- index.js
+++ index.js
@@ -11,14 +11,7 @@ const errorMiddleware = require('./middleware/errorMiddleware');
 
 const app = express();
 app.use(cookieParser());
-app.use(
-    cors({
-        credentials: true,
-        origin: process.env.FRONTEND_URL ?? "http://localhost:4200",
-        // origin: "http://localhost:4200",
-        optionsSuccessStatus: 200,
-    })
-);
+
 // app.use(function (req, res, next) {
 //     //Enabling CORS
 //     res.header("Access-Control-Allow-Origin", "*");
@@ -26,12 +19,32 @@ app.use(
 //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
 //     next();
 // });
-app.all("/*", function(req, res, next){
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+// app.all("/*", function(req, res, next){
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+//     next();
+// });
+let ALLOWED_ORIGINS = ["https://rusprominwest.ru/", "https://xn--b1aghpgdghbiimc.xn--p1ai/"];
+app.use((req, res, next) => {
+    let origin = req.headers.origin;
+    let index = ALLOWED_ORIGINS.indexOf(origin);
+    let theOrigin = (index >= 0) ? origin : ALLOWED_ORIGINS[index];
+
+    res.setHeader("Access-Control-Allow-Origin", theOrigin);
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
     next();
 });
+// app.use(
+//     cors({
+//         credentials: true,
+//         // origin: process.env.FRONTEND_URL ?? "http://localhost:4200",
+//         // origin: "http://localhost:4200",
+//         optionsSuccessStatus: 200,
+//     })
+// );
 
 app.use(express.json());
 app.use('/auth', authRouter);
