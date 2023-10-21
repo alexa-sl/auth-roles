78551b95a862b4e9e3695a28028a29624eb984ad
alexa-sl <>
2023-10-21 13:55:34 +0300

    cors cleaned

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-// const {MongoClient} = require('mongodb');
 const mongoose = require('mongoose').default;
 const authRouter = require('./authRouter');
 const orderRouter = require('./orderRouter');
@@ -13,35 +12,15 @@ const app = express();
 app.use(cookieParser());
 
 app.use(function (req, res, next) {
-    //Enabling CORS
-    // res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
     next();
 });
-// app.all("/*", function(req, res, next){
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-//     next();
-// });
-//let ALLOWED_ORIGINS = ["https://rusprominwest.ru", "https://xn--b1aghpgdghbiimc.xn--p1ai"];
-// app.use((req, res, next) => {
-//     let origin = req.headers.origin;
-//     let index = ALLOWED_ORIGINS.indexOf(origin);
-//     let theOrigin = (index >= 0) ? origin : ALLOWED_ORIGINS[index];
-//
-//     res.setHeader("Access-Control-Allow-Origin", theOrigin);
-//     res.setHeader("Access-Control-Allow-Credentials", "true");
-//     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-//     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
-//     next();
-// });
+
 app.use(
     cors({
         credentials: true,
-        origin: process.env.FRONTEND_URL ?? "http://localhost:4200",
-        // origin: "http://localhost:4200",
+        origin: process.env.FRONTEND_URL,
         optionsSuccessStatus: 200,
     })
 );
@@ -51,11 +30,8 @@ app.use('/auth', authRouter);
 app.use('/orders', orderRouter);
 app.use(errorMiddleware);
 
-// const client = new MongoClient('mongodb://127.0.0.1:27017/rusprom');
-
 const start = async () => {
     try {
-        // client.connect()
         mongoose.connect('mongodb://127.0.0.1:27017/rusprom')
             .then(() => console.log('Connected Successfully'))
             .catch(error => console.log('Failed to connect', error))
